4143fb346f0aff4702b416f82af4ee95c4a3b3da
Stelly <stellytxm@student.usm.my>
2025-04-23 00:41:45 +0800

    Remove firebase

--- firebase/firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase with the service account credentials
-cred = credentials.Certificate("C:\\Users\\User\\Documents\\CareEase\\firebase\\caseease-6397e-firebase-adminsdk-fbsvc-cc704ea4f5.json")
-firebase_admin.initialize_app(cred)
-
-# Access Firestore
-db = firestore.client()
